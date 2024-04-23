e02ff8f4b6e861534795af6fea68e63a626228bf
김용준 <rladydwns129@naver.com>
2024-04-23 23:17:37 +0900

    [Feat] #S10P31S105-92: SSE, FCM Controller, Service 생성
    Change-Id: I16cef98912385092a9fb8089a8114e91994dc3cc

---
+++ be/common/src/main/java/com/sdi/common/api/service/FcmService.java
@@ -0,0 +1,9 @@
+package com.sdi.common.api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FcmService {
+}
