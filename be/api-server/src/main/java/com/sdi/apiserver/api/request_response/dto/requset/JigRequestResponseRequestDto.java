6e6dc80327fecc534f2cf641efa77e37ae236679
강성범 <qkfka9045@gmail.com>
2024-04-23 19:51:24 +0900

    [Fix] #S10P31S105 -75 : 분출 승인시 지그 일련번호되게 전송 수정
    Change-Id: Iff1dccbd8d84f88ae821a8d40ddd34bdd0a680a7

--- be/api-server/src/main/java/com/sdi/apiserver/api/request_response/dto/requset/JigRequestResponseRequestDto.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/request_response/dto/requset/JigRequestResponseRequestDto.java
@@ -2,9 +2,12 @@ package com.sdi.apiserver.api.request_response.dto.requset;
 
 import lombok.Value;
 
+import java.util.List;
+
 @Value
 public class JigRequestResponseRequestDto {
     Long requestId;
     Boolean isAccept;
     String memo;
+    List<String> list; // 지그의 일련번호들
 }
