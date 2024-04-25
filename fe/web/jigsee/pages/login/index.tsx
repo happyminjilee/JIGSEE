726766ce61813e16391b0cabb652e1f57cb72838
MinjiLEE <alswl9703@naver.com>
2024-04-25 10:07:37 +0900

    [Design] #S10P21S105-89: 로그인 화면 디자인 초안 완성
    Change-Id: Id9cc5550ee32821c9c4e31022fdce8f726a8eef7

--- fe/web/jigsee/pages/login/index.tsx
+++ fe/web/jigsee/pages/login/index.tsx
@@ -1,7 +1,25 @@
+import { Input, Button, Checkbox } from "@nextui-org/react";
+import styled from "@/styles/login.module.scss";
+
 export default function Login() {
   return (
     <>
-      <div>Login page</div>
+      <div className={styled.container}>
+        <div className={styled.logincontainer}>
+          <img src="/images/profile.svg" alt="profile-logo" />
+          {/* 로그인 인풋 영역 */}
+          <div style={{ marginBottom: "1rem" }}>
+            <Input type="email" variant={"underlined"} label="사번" />
+          </div>
+          <div style={{ marginBottom: "1rem" }}>
+            <Input type="password" variant={"underlined"} label="비밀번호" />
+          </div>
+          <div style={{ marginBottom: "2rem", justifyContent: "flex-end", display: "flex" }}>
+            <Checkbox defaultSelected>아이디 저장</Checkbox>
+          </div>
+          <Button className={styled.samsungbutton}>로그인</Button>
+        </div>
+      </div>
     </>
   );
 }
