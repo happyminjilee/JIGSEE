206143327d493538f63d490b9eff437800ac3066
MinjiLEE <alswl9703@naver.com>
2024-04-25 21:13:42 +0900

    [Desig] #S10P31S105 - 129: 기술팀  페이지 레이아웃 완성
    Change-Id: Ia818aea410a8cee408839342599ae76061c06d5a

--- fe/web/jigsee/pages/engineer/index.tsx
+++ fe/web/jigsee/pages/engineer/index.tsx
@@ -1,10 +1,14 @@
 import Navbar from "./navbar";
+import styled from "@/styles/engineer.module.scss";
 
 export default function Engineer() {
   return (
     <>
       <Navbar />
-      engineer page 불출내역 화면
+      <div className={styled.container}>
+        <div className={styled.jigrelease}>지그 불출내역 컴포넌트</div>
+        <div className={styled.jigrequest}>불출요청</div>
+      </div>
     </>
   );
 }
--- fe/web/jigsee/pages/engineer/repair.tsx
+++ fe/web/jigsee/pages/engineer/repair.tsx
@@ -1,9 +1,13 @@
 import Navbar from "./navbar";
+import styled from "@/styles/engineer.module.scss";
 export default function Repair() {
   return (
     <>
       <Navbar />
-      engineer Repair page 보수내역 수리 화면
+      <div className={styled.container}>
+        <div className={styled.jigrepairlist}>지그 수리 대기</div>
+        <div className={styled.repairrequest}>지그 수리 요청</div>
+      </div>
     </>
   );
 }
