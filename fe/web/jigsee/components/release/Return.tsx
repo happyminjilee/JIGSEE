24dbd5650c67e7f03dc28658790fdb80585fb383
MinjiLEE <alswl9703@naver.com>
2024-04-26 17:47:34 +0900

    [Feat] #S10P31S105-130: jig불출 승인 반려 모달 기능연결
    Change-Id: I4c6258985de1bdcb026ba9743ebf3aab9f4fbf00

--- fe/web/jigsee/components/release/Return.tsx
+++ fe/web/jigsee/components/release/Return.tsx
@@ -1,3 +1,32 @@
-export default function Return() {
-  return <>Return 불출 반려</>;
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@nextui-org/react";
+import { Divider, Textarea } from "@nextui-org/react";
+
+// Props에 대한 인터페이스 정의
+interface ReturnProps {
+  onClose: () => void; // 이 함수는 파라미터를 받지 않고 void를 반환함
+}
+export default function Return({ onClose }: ReturnProps) {
+  return (
+    <Modal isOpen={true} onClose={onClose}>
+      <ModalContent>
+        <ModalHeader className="flex flex-col gap-1">
+          불출 반려
+          <Divider />
+          승인 요청 제목 날짜 영역
+        </ModalHeader>
+        <ModalBody>
+          <Textarea
+            isReadOnly
+            variant="flat"
+            placeholder="승인 내역"
+            defaultValue="모델명 수량 NextUI is a React UI library that provides a set of accessible, reusable, and beautiful components."
+            className="max-w-xs"
+          />
+        </ModalBody>
+        <ModalFooter>
+          <button onClick={onClose}>확인</button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
--- fe/web/jigsee/pages/manager/index.tsx
+++ fe/web/jigsee/pages/manager/index.tsx
@@ -1,12 +1,28 @@
 import Navbar from "./navbar";
+import React, { useState } from "react";
 import styled from "@/styles/manager.module.scss";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import RequestList from "@/components/release/RequestList";
+import Approve from "@/components/release/Approve";
+import Return from "@/components/release/Return";
+
 export default function Manager() {
+  const [showApproveModal, setApproveShowModal] = useState(false);
+  const [showReturnModal, setReturnShowModal] = useState(false);
+
+  const openApproveModal = () => setApproveShowModal(true);
+  const closeApproveModal = () => setApproveShowModal(false);
+  const openReturnModal = () => setReturnShowModal(true);
+  const closeReturnModal = () => setReturnShowModal(false);
   return (
     <>
       <Navbar />
       <div className={styled.container}>
-        <div className={styled.releasecontainer}>불출요청내역 컴포넌트 자리</div>
+        <div className={styled.releasecontainer}>
+          <RequestList onApproveClick={openApproveModal} onReturnClick={openReturnModal} />
+          {showApproveModal && <Approve onClose={closeApproveModal} />}
+          {showReturnModal && <Return onClose={closeReturnModal} />}
+        </div>
         <div className={styled.stockcontainer}>재고현황리스트</div>
         <div className={styled.btncontainer}>
           <button className={styled.jigbtn}>
