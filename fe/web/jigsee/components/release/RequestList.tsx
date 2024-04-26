e8f652c8757e29b34cd4fafc17b6b41c1623575d
MinjiLEE <alswl9703@naver.com>
2024-04-26 17:47:34 +0900

    [Feat] #S10P31S105-130: jig불출 승인 반려 모달 기능연결
    Change-Id: I4c6258985de1bdcb026ba9743ebf3aab9f4fbf00

--- fe/web/jigsee/components/release/RequestList.tsx
+++ fe/web/jigsee/components/release/RequestList.tsx
@@ -1,66 +1,78 @@
-import {useState} from "react";
-import {Link, Button} from "@nextui-org/react";
-import styled from "@/styles/requestlist.module.css"
-import {list} from "postcss";
-
+import { useState } from "react";
+import { Link, Button } from "@nextui-org/react";
+import styled from "@/styles/requestlist.module.css";
+import { list } from "postcss";
 
 interface lst {
-    index: number
-    date: string
-    title: string
-    content: string[]
+  index: number;
+  date: string;
+  title: string;
+  content: string[];
 }
-
-
-export default function RequestList() {
-    const lst= [
-        {date: '2024.04.26', title: '생산팀인데 지그 주세요', content: ['지그 목록', '지그 목록', '지그 목록', '지그 목록', '지그 목록', '지그 목록']},
-        {date: '2024.04.25', title: '생산팀인데 지그 주세요', content: ['지그 목록', '지그 목록', '지그 목록']},
-        {date: '2024.04.24', title: '생산팀인데 지그 주세요', content: ['지그 목록', '지그 목록', '지그 목록']},
-    ]
+interface RequestListProps {
+  onApproveClick: () => void;
+  onReturnClick: () => void;
+}
+export default function RequestList({ onApproveClick, onReturnClick }: RequestListProps) {
+  const lst = [
+    {
+      date: "2024.04.26",
+      title: "생산팀인데 지그 주세요",
+      content: ["지그 목록", "지그 목록", "지그 목록", "지그 목록", "지그 목록", "지그 목록"],
+    },
+    {
+      date: "2024.04.25",
+      title: "생산팀인데 지그 주세요",
+      content: ["지그 목록", "지그 목록", "지그 목록"],
+    },
+    {
+      date: "2024.04.24",
+      title: "생산팀인데 지그 주세요",
+      content: ["지그 목록", "지그 목록", "지그 목록"],
+    },
+  ];
   return (
-      <>
-          <div className={styled.box}>
-              <div style={{display: "flex", justifyContent: "space-between"}}>
-                  <div style={{fontWeight: "bold", fontSize: "20px"}}>불출 요청</div>
-                  <Link
-                      href="#"
-                      underline="hover"
-                      style={{color: "black"}}
-                  >
-                      상세 보기
-                  </Link>
+    <>
+      <div className={styled.box}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ fontWeight: "bold", fontSize: "20px" }}>불출 요청</div>
+          <Link href="#" underline="hover" style={{ color: "black" }}>
+            상세 보기
+          </Link>
+        </div>
+
+        {/*내부 박스*/}
+        {lst.map((info, index) => (
+          <div className={styled.container}>
+            <div style={{ marginTop: "10px", marginLeft: "15px" }}>{info.date}</div>
+            <div className={styled.card}>
+              <div key={index} className={styled.division}>
+                <div style={{ fontWeight: "bold", fontSize: "30px" }}>{info.title}</div>
+                <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+                  {info.content.map((index) => (
+                    <div style={{ margin: "2px" }}> {index} </div>
+                  ))}
+                </div>
               </div>
-
-              {/*내부 박스*/}
-              {lst.map((info, index) => (
-                  <div className={styled.container}>
-                      <div style={{marginTop: "10px", marginLeft: "15px"}}>{info.date}</div>
-                      <div className={styled.card}>
-                          <div
-                              key={index}
-                              className={styled.division}
-                          >
-                              <div style={{fontWeight: "bold", fontSize: "30px"}}>{info.title}</div>
-                              <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap"}}>
-                                  {info.content.map((index) => (
-                                      <div style={{margin: "2px"}}> {index} </div>
-                                  ))}
-                              </div>
-
-                          </div>
-                          <div
-                              className={styled.division}
-                          >
-                              <Button size="lg" color="primary" style={{fontWeight: "bold", marginBottom: "5px"}}>승인</Button>
-                              <Button size="lg" style={{fontWeight: "bold"}}>반려</Button>
-                          </div>
-                      </div>
-                      <div></div>
-                      <div></div>
-                  </div>
-              ))}
+              <div className={styled.division}>
+                <Button
+                  size="lg"
+                  color="primary"
+                  style={{ fontWeight: "bold", marginBottom: "5px" }}
+                  onClick={onApproveClick}
+                >
+                  승인
+                </Button>
+                <Button size="lg" style={{ fontWeight: "bold" }} onClick={onReturnClick}>
+                  반려
+                </Button>
+              </div>
+            </div>
+            <div></div>
+            <div></div>
           </div>
-      </>
-  )
+        ))}
+      </div>
+    </>
+  );
 }
