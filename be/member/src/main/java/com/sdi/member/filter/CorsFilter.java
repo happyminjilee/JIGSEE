038680c41c2ac1af8c7ea0658dc105ace6b40ea8
차현철 <ckguscjf03@gmail.com>
2024-04-29 20:29:45 +0900

    [Fix] #S10P31S105: CORS 관련 모든 도메인 허용 추후 삭제 예정
    Change-Id: I8d87cb426e2ac43deac1eafae1e9a9fbb315a4fa

--- be/member/src/main/java/com/sdi/member/filter/CorsFilter.java
+++ be/member/src/main/java/com/sdi/member/filter/CorsFilter.java
@@ -1,2 +1,33 @@
-package com.sdi.member.filter;public class CorsFilter {
+package com.sdi.member.filter;
+
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException, java.io.IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "false");
+        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, Authorization, RefreshToken");
+
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }else {
+            chain.doFilter(req, res);
+        }
+    }
 }
