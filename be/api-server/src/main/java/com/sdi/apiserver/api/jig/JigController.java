8c45e86332128a706a0e25da01f35847454d882f
강성범 <qkfka9045@gmail.com>
2024-04-29 11:10:26 +0900

    [Feat] #S10P31S105: log 추가
    Change-Id: I65d7003d5882f25dea042cfe45b71d1d3b058609

--- be/api-server/src/main/java/com/sdi/apiserver/api/jig/JigController.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/jig/JigController.java
@@ -5,6 +5,7 @@ import com.sdi.apiserver.api.jig.dto.request.JigUpdateRequestDto;
 import com.sdi.apiserver.api.jig.dto.response.JigResponseDto;
 import com.sdi.apiserver.util.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/jig")
 @RequiredArgsConstructor
+@Slf4j
 class JigController {
 
     @Value("${apis.jig-base-url}")
@@ -21,12 +23,14 @@ class JigController {
 
     @PutMapping
     ResponseEntity<Response<Void>> update(@RequestBody JigUpdateRequestDto dto) {
+        log.info("{}의 check list 수정", dto.getModel());
         String url = String.format("%s/v1/jig", jigBaseUrl);
         return request.put(url, dto);
     }
 
     @GetMapping
     public ResponseEntity<Response<JigResponseDto>> searchByModel(@RequestParam(name = "model") String model) {
+        log.info("{} 정보 조회 요청", model);
         String url = String.format("%s/v1/jig?model=%s", jigBaseUrl, model);
         return request.get(url);
     }
--- be/api-server/src/main/java/com/sdi/apiserver/api/jig/JigItemController.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/jig/JigItemController.java
@@ -9,6 +9,7 @@ import com.sdi.apiserver.api.jig.dto.response.JigItemIsUsableResponseDto;
 import com.sdi.apiserver.api.jig.dto.response.JigItemResponseDto;
 import com.sdi.apiserver.util.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/jig-item")
 @RequiredArgsConstructor
+@Slf4j
 class JigItemController {
 
     @Value("${apis.jig-base-url}")
@@ -23,14 +25,16 @@ class JigItemController {
 
     private final CommonRequest request;
 
-    @GetMapping()
+    @GetMapping
     ResponseEntity<Response<JigItemResponseDto>> findBySerialNo(@RequestParam(name = "serial-no") String serialNo) {
+        log.info("{}로 Jig-Item정보 조회", serialNo);
         String url = String.format("%s/v1/jig-item?serial-no=%s", jigBaseUrl, serialNo);
         return request.get(url);
     }
 
     @PostMapping()
     ResponseEntity<Response<Void>> add(@RequestBody JigItemAddRequestDto dto) {
+        log.info("Jig-Item 데이터 삽입{}", dto.list());
         String url = String.format("%s/v1/jig-item", jigBaseUrl);
         return request.post(url, dto);
     }
@@ -38,30 +42,35 @@ class JigItemController {
     @GetMapping("/usable")
     ResponseEntity<Response<JigItemIsUsableResponseDto>> isUsable(@RequestParam(name = "facility-model") String facilityModel,
                                                   @RequestParam(name = "jig-serial-no") String jigSerialNo) {
+        log.info("{}에 {}가 들어갈 수 있는지 확인", facilityModel, jigSerialNo);
         String url = String.format("%s/v1/jig-item/usable?facility-model=%s&jig-serial-no=%s", jigBaseUrl, facilityModel, jigSerialNo);
         return request.get(url);
     }
 
     @DeleteMapping()
     ResponseEntity<Response<Void>> delete(@RequestBody JigItemSerialNoRequestDto dto) {
+        log.info("{} 삭제", dto.serialNo());
         String url = String.format("%s/v1/jig-item", jigBaseUrl);
         return request.delete(url, dto);
     }
 
     @PutMapping("/status")
     ResponseEntity<Response<Void>> updateStatus(@RequestBody JigItemUpdateStatusRequestDto dto){
+        log.info("{} 상태 변경 {}", dto.serialNo(), dto.status());
         String url = String.format("%s/v1/jig-item/status", jigBaseUrl);
         return request.put(url, dto);
     }
 
     @PutMapping("/exchange")
     ResponseEntity<Response<Void>> exchange(@RequestBody JigItemExchangeRequestDto dto){
+        log.info("{}에 {}를 {}로 변경", dto.facilitySerialNo(), dto.beforeSerialNo(), dto.afterSerialNo());
         String url = String.format("%s/v1/jig-item/exchange", jigBaseUrl);
         return request.put(url, dto);
     }
 
     @PutMapping("/recovery")
     ResponseEntity<Response<Void>> recovery(@RequestBody JigItemSerialNoRequestDto dto) {
+        log.info("{} 상태 변경", dto.serialNo());
         String url = String.format("%s/v1/jig-item/recovery", jigBaseUrl);
         return request.put(url, dto);
     }
