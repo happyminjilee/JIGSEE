a67c4a497ca662e66fdc6f3cdac5177c7bb3af19
차현철 <ckguscjf03@gmail.com>
2024-04-30 11:16:04 +0900

    [Fix] #S10P31S105: CORS 관련 모든 도메인 허용 추후 삭제 예정
    Change-Id: I61c117937de453dcc05da175a59e52768ad821e9

--- be/member/src/main/java/com/sdi/member/filter/CorsFilter.java
+++ be/member/src/main/java/com/sdi/member/filter/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization, RefreshToken");
-        response.setHeader("Access-Control-Expose-Headers", "*");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization, RefreshToken");
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
