eda739e292c260fa1a729542cd66da3cb34bb12d
강성범 <qkfka9045@gmail.com>
2024-05-01 19:49:00 +0900

    [Feat] #S10P31S105-134: yml 키 누락 수정
    Change-Id: Ib2d8dffc61bb6c9ff0f30dd8d6480343c65f2f14

--- be/work-order/src/main/java/com/sdi/work_order/client/JigItemClient.java
+++ be/work-order/src/main/java/com/sdi/work_order/client/JigItemClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "jigItemClient", url = "${api-base-url}")
+@FeignClient(name = "jigItemClient", url = "${apis.api-base-url}")
 public interface JigItemClient {
 
     @GetMapping("/jig-item")
