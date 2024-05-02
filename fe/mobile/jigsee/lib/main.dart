1f1337ae44bccbe49e7d3f0ec5be7db3d212052a
박수형 <nastorond@kakao.com>
2024-05-02 13:12:49 +0900

    [Feat] #S10P31S105-156: 화면 넘어오면 카메라 켜지고 ocr 결과 화면출력
    Change-Id: I23424e14784346788897fc3d05711785308d1234

--- fe/mobile/jigsee/lib/main.dart
+++ fe/mobile/jigsee/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jigsee/pages/home_page.dart';
@@ -5,12 +6,21 @@ import 'package:jigsee/pages/login_page.dart';
 import 'package:jigsee/components/navigate_animation.dart';
 import 'package:jigsee/pages/ocr_page.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  runApp(ProviderScope(child: MyApp(
+      camera: firstCamera,
+  )));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.camera}) : super(key: key);
+
+  final CameraDescription camera;
 
   // This widget is the root of your application.
   @override
@@ -38,7 +48,9 @@ class MyApp extends StatelessWidget {
           case '/login':
             return NoAnimationRoute(builder: (_) => const LoginPage(), settings: settings);
           case '/ocr':
-            return NoAnimationRoute(builder: (_) => const ReadSerialNum(), settings: settings);
+            return NoAnimationRoute(builder: (_) => ReadSerialNum(
+              camera: camera,
+            ), settings: settings);
           default:
             return MaterialPageRoute(builder: (_) => const HomePage());
         }
--- fe/mobile/jigsee/lib/pages/ocr_page.dart
+++ fe/mobile/jigsee/lib/pages/ocr_page.dart
@@ -1,119 +1,126 @@
 import 'dart:async';
-import 'dart:developer';
 import 'dart:io';
 
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_scalable_ocr/flutter_scalable_ocr.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:jigsee/components/header.dart';
 
 class ReadSerialNum extends StatefulWidget {
-  const ReadSerialNum({Key? key}) : super(key: key);
+  const ReadSerialNum({Key? key, required this.camera}) : super(key: key);
+  final CameraDescription camera;
 
   @override
   State<ReadSerialNum> createState() => _ReadSerialNumState();
 }
 
 class _ReadSerialNumState extends State<ReadSerialNum> {
-  XFile? _image; //이미지를 담을 변수 선언
-  final ImagePicker picker = ImagePicker(); //ImagePicker 초기화
-  String scannedText = "";  // textRecognizer로 인식된 텍스트를 담을 String
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
-  //이미지를 가져오는 함수
-  Future getImage(ImageSource imageSource) async {
-    //pickedFile에 ImagePicker로 가져온 이미지가 담긴다.
-    final XFile? pickedFile = await picker.pickImage(source: imageSource);
-    if (pickedFile != null) {
-      setState(() {
-        _image = XFile(pickedFile.path); //가져온 이미지를 _image에 저장
-      });
-      getRecognizedText(_image!); // 이미지를 가져온 뒤 텍스트 인식 실행
-    }
+  @override
+  void initState() {
+    super.initState();
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+    _initializeControllerFuture = _controller.initialize();
   }
 
-  void getRecognizedText(XFile image) async {
-    // XFile 이미지를 InputImage 이미지로 변환
-    final InputImage inputImage = InputImage.fromFilePath(image.path);
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
-    // textRecognizer 초기화, 이때 script에 인식하고자하는 언어를 인자로 넘겨줌
-    // ex) 영어는 script: TextRecognitionScript.latin, 한국어는 script: TextRecognitionScript.korean
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return CameraPreview(_controller);
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            }),
+          TextButton(onPressed: () async {
+            try {
+              await _initializeControllerFuture;
+              final image = await _controller.takePicture();
+              if(!mounted) return;
+              await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => DisplayPictureScreen (
+                    imagePath: image.path,
+                  )
+                )
+              );
+            } catch (e) {
+              if (kDebugMode) {
+                print(e);
+              }
+            };
+          }, child: const Text("촬영"),)
+        ],
+      )
+    );
+  }
+}
+
+class DisplayPictureScreen extends StatefulWidget {
+  final String imagePath;
+  const DisplayPictureScreen({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _DisplayPictureScreenState();
+}
+
+class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
+  late String scannedText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getRecognizedText();
+  }
+
+  void getRecognizedText() async {
+    final InputImage inputImage = InputImage.fromFilePath(widget.imagePath);
+
     final textRecognizer =
     GoogleMlKit.vision.textRecognizer(script: TextRecognitionScript.latin);
 
-    // 이미지의 텍스트 인식해서 recognizedText에 저장
     RecognizedText recognizedText =
     await textRecognizer.processImage(inputImage);
 
-    // Release resources
     await textRecognizer.close();
 
-    // 인식한 텍스트 정보를 scannedText에 저장
-    scannedText = "";
     for (TextBlock block in recognizedText.blocks) {
       for (TextLine line in block.lines) {
         scannedText = scannedText + line.text + "\n";
       }
     }
-
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: const CustomAppBar(),
         body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(height: 30, width: double.infinity),
-            _buildPhotoArea(),
-            _buildRecognizedText(),
+            const Text('지그 정보'),
+            Image.file(File(widget.imagePath)),
             const SizedBox(height: 20),
-            _buildButton(),
+            Text(scannedText == "" ? "Error" : scannedText),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildPhotoArea() {
-    return _image != null
-        ? SizedBox(
-          width: 300,
-          height: 300,
-          child: Image.file(File(_image!.path)), //가져온 이미지를 화면에 띄워주는 코드
-      )
-        : Container(
-      width: 300,
-      height: 300,
-      color: Colors.grey,
-    );
-  }
-
-  Widget _buildRecognizedText() {
-    return Text(scannedText); //getRecognizedText()에서 얻은 scannedText 값 출력
-  }
-
-  Widget _buildButton() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            getImage(ImageSource.camera); //getImage 함수를 호출해서 카메라로 찍은 사진 가져오기
-          },
-          child: const Text("카메라"),
-        ),
-        const SizedBox(width: 30),
-        ElevatedButton(
-          onPressed: () {
-            getImage(ImageSource.gallery); //getImage 함수를 호출해서 갤러리에서 사진 가져오기
-          },
-          child: const Text("갤러리"),
-        ),
-      ],
+        )
     );
   }
 }
