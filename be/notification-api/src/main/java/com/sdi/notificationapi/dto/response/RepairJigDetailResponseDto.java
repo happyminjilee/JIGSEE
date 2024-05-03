5408288cbc0a8ce576dcab6f83de1d0e39a61629
김용준 <rladydwns129@naver.com>
2024-05-03 10:25:45 +0900

    [Fix] #S10P31S105-115: response 형태에 맞게 ResponseDto 수정
    Change-Id: I0661241f10f05dc9d2973c1d8a5ab2c5d88737d2

--- be/notification-api/src/main/java/com/sdi/notificationapi/dto/response/RepairJigDetailResponseDto.java
+++ be/notification-api/src/main/java/com/sdi/notificationapi/dto/response/RepairJigDetailResponseDto.java
@@ -7,12 +7,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record RepairJigDetailResponseDto(
-        String id,
-        String from,
-        @JsonProperty("createdAt")
-        LocalDateTime date,
-        String memo,
-        List<String> list
+        String id, // uuid
+        String from, // 요청자
+        LocalDateTime createdAt, // 요청 시간
+        String memo, // 보수 요청 메모
+        List<String> list // 지그 일련번호 리스트
 ) {
     public static RepairJigDetailResponseDto from(RepairRequestEntity entity) {
         return new RepairJigDetailResponseDto(
