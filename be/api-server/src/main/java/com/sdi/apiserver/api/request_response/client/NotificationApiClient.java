99febad9bcfba5ada671d02c4de91575651294bf
김용준 <rladydwns129@naver.com>
2024-05-03 13:21:01 +0900

    [Feat] #S10P31S105-173: notification-api 서버 연결
    Change-Id: Ia353804a14c3f9496ce78b7cc50c2ce9be5b2d64

--- be/api-server/src/main/java/com/sdi/apiserver/api/request_response/client/NotificationApiClient.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/request_response/client/NotificationApiClient.java
@@ -1,8 +1,47 @@
 package com.sdi.apiserver.api.request_response.client;
 
+import com.sdi.apiserver.api.request_response.dto.request.RepairJigRequestDto;
+import com.sdi.apiserver.api.request_response.dto.request.RequestJigRequestDto;
+import com.sdi.apiserver.api.request_response.dto.request.ResponseJigRequestDto;
+import com.sdi.apiserver.api.request_response.dto.response.RepairJigDetailResponseDto;
+import com.sdi.apiserver.api.request_response.dto.response.RepairJigListResponseDto;
+import com.sdi.apiserver.api.request_response.dto.response.RequestJigDetailResponseDto;
+import com.sdi.apiserver.api.request_response.dto.response.RequestJigListResponseDto;
+import com.sdi.apiserver.util.Response;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "notificationApiClient", url = "${apis.notification-api-base-url}")
 public interface NotificationApiClient {
+    final String ACCESS_TOKEN_PREFIX = "Authorization";
+    @PostMapping("request/jig")
+    Response<Void> makeWantRequest(@RequestHeader(name = ACCESS_TOKEN_PREFIX) String accessToken,
+                                   @RequestBody RequestJigRequestDto dto);
 
+    @PostMapping("/response/jig")
+    Response<Void> makeWantResponse(@RequestHeader(name = ACCESS_TOKEN_PREFIX) String accessToken,
+                                    @RequestBody ResponseJigRequestDto dto);
+
+    @PostMapping("/request/repair")
+    Response<Void> makeRepairRequest(@RequestHeader(name = ACCESS_TOKEN_PREFIX) String accessToken,
+                                     @RequestBody RepairJigRequestDto dto);
+
+    @GetMapping("/request/jig/all")
+    Response<RequestJigListResponseDto> findAllWantJigRequests(@RequestParam(value = "filter",defaultValue = "ALL") String filter,
+                                                               @RequestParam(value = "page", defaultValue = "1") int page,
+                                                               @RequestParam(value = "size", defaultValue = "10") int size);
+
+    @GetMapping("/request/jig/detail")
+    Response<RequestJigDetailResponseDto> findOneJigRequest(@RequestParam(value = "request-jig-id") String requestId);
+
+    @GetMapping("/request/repair")
+    Response<RepairJigListResponseDto> findAllRepairRequests(@RequestParam(value = "page", defaultValue = "1") int page,
+                                                             @RequestParam(value = "size", defaultValue = "10") int size);
+
+    @GetMapping("/request/repair/detail")
+    Response<RepairJigDetailResponseDto> findOneRepairRequest(@RequestParam(value = "repair-jig-id") String requestId);
 }
--- be/api-server/src/main/java/com/sdi/apiserver/api/request_response/dto/request/RequestJigRequestDto.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/request_response/dto/request/RequestJigRequestDto.java
@@ -1,16 +1,12 @@
 package com.sdi.apiserver.api.request_response.dto.request;
 
-import lombok.Value;
-
 import java.util.List;
 
-@Value
-public class RequestJigRequestDto {
-    List<JigRequest> list;
-
-    public static class JigRequest{
-        String to;
-        String model;
-        Integer count;
-    }
+/**
+ *
+ * @param serialNos 요청 지그 일련번호 리스트
+ */
+public record RequestJigRequestDto(
+        List<String> serialNos // 요청 지그 리스트
+) {
 }
