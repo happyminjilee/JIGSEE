60fef35bf5fa831dffc00228cdbf9dc0593b1e9a
차현철 <ckguscjf03@gmail.com>
2024-05-03 21:32:09 +0900

    [Fix] #S10P31S105: 동일 빈 이름 긴급수정
    Change-Id: I61189facb48db652086038789c8b6d27d4b05aec

--- be/api-server/src/main/java/com/sdi/apiserver/api/notification/FcmController.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/notification/FcmController.java
@@ -1,20 +1,8 @@
 package com.sdi.apiserver.api.notification;
 
-import com.sdi.apiserver.api.request_response.client.NotificationApiClient;
-import com.sdi.apiserver.api.request_response.dto.request.RepairJigRequestDto;
-import com.sdi.apiserver.api.request_response.dto.request.RequestJigRequestDto;
-import com.sdi.apiserver.api.request_response.dto.response.RepairJigDetailResponseDto;
-import com.sdi.apiserver.api.request_response.dto.response.RepairJigListResponseDto;
-import com.sdi.apiserver.api.request_response.dto.response.RequestJigDetailResponseDto;
-import com.sdi.apiserver.api.request_response.dto.response.RequestJigListResponseDto;
-import com.sdi.apiserver.util.Response;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
