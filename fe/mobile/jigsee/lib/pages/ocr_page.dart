55c7315f693e354da6740b3a3bdfd1d25e50fc82
박수형 <nastorond@kakao.com>
2024-05-07 01:39:22 +0900

    [Feat] #S10P31S105-160: ocr 후 뒤로가기 기능 추가
    Change-Id: I3f89db817f77464f10e3a4d04a4c8c317c8f2b63

--- fe/mobile/jigsee/lib/pages/ocr_page.dart
+++ fe/mobile/jigsee/lib/pages/ocr_page.dart
@@ -10,6 +10,7 @@ import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:jigsee/components/header.dart';
 import 'package:jigsee/api/provider.dart';
 import 'package:jigsee/api/dio_instance.dart';
+import 'package:jigsee/pages/spe_jig_list.dart';
 
 class ReadSerialNum extends StatefulWidget {
   const ReadSerialNum({Key? key, required this.camera}) : super(key: key);
@@ -77,64 +78,6 @@ class _ReadSerialNumState extends State<ReadSerialNum> {
   }
 }
 
-// class DisplayPictureScreen extends ConsumerStatefulWidget {
-//   final String imagePath;
-//   const DisplayPictureScreen({Key? key, required this.imagePath}) : super(key: key);
-//
-//   @override
-//   ConsumerState<ConsumerStatefulWidget> createState() => _DisplayPictureScreenState();
-// }
-//
-// class _DisplayPictureScreenState extends ConsumerState<DisplayPictureScreen> {
-//   late String scannedText = "";
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     getRecognizedText();
-//   }
-//
-//   void getRecognizedText() async {
-//     final InputImage inputImage = InputImage.fromFilePath(widget.imagePath);
-//
-//     final textRecognizer =
-//     GoogleMlKit.vision.textRecognizer(script: TextRecognitionScript.latin);
-//
-//     RecognizedText recognizedText =
-//     await textRecognizer.processImage(inputImage);
-//
-//     await textRecognizer.close();
-//
-//     for (TextBlock block in recognizedText.blocks) {
-//       for (TextLine line in block.lines) {
-//         scannedText = scannedText + line.text + "\n";
-//       }
-//     }
-//     setState(() {});
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final jigName = ref.watch(selectedJigProvider);
-//     return Scaffold(
-//         appBar: const CustomAppBar(),
-//         body: Column(
-//           children: [
-//             const Text('지그 정보'),
-//             Container(
-//               alignment: Alignment.center,
-//               height: 500,
-//               padding: const EdgeInsets.all(12),
-//               child: Image.file(File(widget.imagePath)),
-//             ),
-//             const SizedBox(height: 20),
-//             Text(jigName),
-//             Text(scannedText == "" ? "Error" : scannedText),
-//           ],
-//         )
-//     );
-//   }
-// }
 class DisplayPictureScreen extends ConsumerStatefulWidget {
   final String imagePath;
   const DisplayPictureScreen({Key? key, required this.imagePath}) : super(key: key);
@@ -202,29 +145,62 @@ class _DisplayPictureScreenState extends ConsumerState<DisplayPictureScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: Column(
-        children: [
-          const Text('Jig Information'),
-          Expanded(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const Text('지그 정보'),
+            Expanded(
               child: Column(
-                children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 228,
-                  padding: const EdgeInsets.all(12),
-                  child: Image.file(File(widget.imagePath)),
-                ),
-                const SizedBox(height: 20),
-                Text(ref.watch(selectedJigProvider)),
-                Text(scannedText.isEmpty ? "Error scanning" : scannedText),
-                if (verificationImagePath != null) SvgPicture.asset('assets/$verificationImagePath.svg'),
-                if (verificationImagePath == "availableMark") const Text('사용 가능')
-                else const Text('사용 할 수 없는 지그 입니다.')
-              ]
-            ),
-          )
-        ],
-      ),
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(12),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 47, 118, 255),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        children: [
+                          Image.file(File(widget.imagePath), scale: 2.5,),
+                          const SizedBox(height: 20),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(ref.watch(selectedJigProvider)),
+                              Text(scannedText.isEmpty ? "Error scanning" : scannedText),
+                            ]
+                          )
+                        ],
+                      ),
+                    ),
+                    if (verificationImagePath != null) SvgPicture.asset('assets/$verificationImagePath.svg'),
+                    if (verificationImagePath == "availableMark") const Text('사용 가능')
+                    else const Text('사용 할 수 없는 지그 입니다.'),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const SpeJigList()));
+                      },
+                      child: const Text('확인', style: TextStyle(fontSize: 20)),
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(300, 50),
+                          foregroundColor: const Color.fromARGB(255, 248, 250, 252),
+                          backgroundColor: const Color.fromARGB(255, 47, 118, 255),
+                          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          )
+                      ),
+                    ),
+                  ]
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
