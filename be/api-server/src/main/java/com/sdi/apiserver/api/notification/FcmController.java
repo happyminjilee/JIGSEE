9259cadc3621407365b53aa6c5ddca83137a6e78
김용준 <rladydwns129@naver.com>
2024-05-07 11:14:27 +0900

    [Feat] #S10P31S105-172: 추가 api 연결
    Change-Id: I94e00ed1d11b5b84f015a4bf038e79b7de4c35fe

--- be/api-server/src/main/java/com/sdi/apiserver/api/notification/FcmController.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/notification/FcmController.java
@@ -1,20 +1,14 @@
 package com.sdi.apiserver.api.notification;
 
-import com.sdi.apiserver.api.request_response.client.NotificationApiClient;
-import com.sdi.apiserver.api.request_response.dto.request.RepairJigRequestDto;
-import com.sdi.apiserver.api.request_response.dto.request.RequestJigRequestDto;
-import com.sdi.apiserver.api.request_response.dto.response.RepairJigDetailResponseDto;
-import com.sdi.apiserver.api.request_response.dto.response.RepairJigListResponseDto;
-import com.sdi.apiserver.api.request_response.dto.response.RequestJigDetailResponseDto;
-import com.sdi.apiserver.api.request_response.dto.response.RequestJigListResponseDto;
+import com.sdi.apiserver.api.notification.client.NotificationClient;
+import com.sdi.apiserver.api.notification.dto.request.FcmTokenRequestDto;
+import com.sdi.apiserver.api.notification.dto.request.NotificationFcmInspectionRequestDto;
 import com.sdi.apiserver.util.Response;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,6 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FcmController {
     public static final String ACCESS_TOKEN_PREFIX = "Authorization";
+    private final NotificationClient notificationClient;
+
+    @PostMapping("/token")
+    Response<Void> saveToken(HttpServletRequest request, @RequestBody FcmTokenRequestDto fcmTokenRequestDto) {
+        notificationClient.saveToken(getAccessToken(request), fcmTokenRequestDto);
+        return Response.success();
+    }
+
+    @PostMapping("/inspection")
+    Response<Void> sendFcmNotification(@RequestBody NotificationFcmInspectionRequestDto notificationFcmInspectionRequestDto) {
+        notificationClient.sendInspectionNotification(notificationFcmInspectionRequestDto);
+        return Response.success();
+    }
 
     private String getAccessToken(HttpServletRequest request) {
         return request.getHeader(ACCESS_TOKEN_PREFIX);
--- be/api-server/src/main/java/com/sdi/apiserver/api/notification/client/NotificationClient.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/notification/client/NotificationClient.java
@@ -1,6 +1,8 @@
 package com.sdi.apiserver.api.notification.client;
 
+import com.sdi.apiserver.api.notification.dto.request.FcmTokenRequestDto;
 import com.sdi.apiserver.api.notification.dto.request.MessageRequestDto;
+import com.sdi.apiserver.api.notification.dto.request.NotificationFcmInspectionRequestDto;
 import com.sdi.apiserver.api.notification.dto.response.NotificationListResponseDto;
 import com.sdi.apiserver.api.notification.dto.response.UncheckedNotificationListResponseDto;
 import com.sdi.apiserver.util.Response;
@@ -13,26 +15,30 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@FeignClient(name = "notificationApiClient", url = "${apis.notification-base-url}")
+@FeignClient(name = "notificationClient", url = "${apis.notification-base-url}")
 public interface NotificationClient {
     final String ACCESS_TOKEN_PREFIX = "Authorization";
 
-    @GetMapping(value = "notification/sse/subscribe", produces = "text/event-stream")
+    @GetMapping(value = "/notification/sse/subscribe", produces = "text/event-stream")
     SseEmitter subscribe(@RequestHeader(ACCESS_TOKEN_PREFIX) String accessToken);
 
-    @PostMapping(value = "notification/sse/send-message")
+    @PostMapping(value = "/notification/sse/send-message")
     Response<Void> sendMessage(@RequestBody MessageRequestDto messageRequestDto);
 
-    @DeleteMapping("notification/sse/disconnect")
+    @DeleteMapping("/notification/sse/disconnect")
     Response<Void> disconnect(@RequestHeader(ACCESS_TOKEN_PREFIX) String accessToken);
 
-    @GetMapping("notification/search/unchecked")
+    @GetMapping("/notification/search/unchecked")
     Response<UncheckedNotificationListResponseDto> searchUnchecked(@RequestHeader(ACCESS_TOKEN_PREFIX) String accessToken);
 
-    @GetMapping("notification/search/all")
+    @GetMapping("/notification/search/all")
     Response<NotificationListResponseDto> searchAll(@RequestHeader(ACCESS_TOKEN_PREFIX) String accessToken,
                                                     @RequestParam(value = "page", defaultValue = "1") int page,
                                                     @RequestParam(value = "size", defaultValue = "10") int size);
 
+    @PostMapping(value = "/notification/fcm/token")
+    Response<Void> saveToken(@RequestHeader(ACCESS_TOKEN_PREFIX) String accessToken, @RequestBody FcmTokenRequestDto fcmTokenRequestDto);
 
+    @PostMapping(value = "/notification/fcm/inspection")
+    Response<Void> sendInspectionNotification(@RequestBody NotificationFcmInspectionRequestDto dto);
 }
