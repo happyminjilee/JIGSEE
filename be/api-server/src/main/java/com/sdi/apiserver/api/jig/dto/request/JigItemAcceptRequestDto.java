e82e136f4b97fb50c00824bd0dbd7cf6a352ef26
강성범 <qkfka9045@gmail.com>
2024-05-07 13:53:20 +0900

    [Fix] #S10P31S105: 지그 불출 승인 요청 변경
    Change-Id: I480b08cf915efe8d12287719a3ac31405a57dd66

--- be/api-server/src/main/java/com/sdi/apiserver/api/jig/dto/request/JigItemAcceptRequestDto.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/jig/dto/request/JigItemAcceptRequestDto.java
@@ -3,6 +3,9 @@ package com.sdi.apiserver.api.jig.dto.request;
 import java.util.List;
 
 public record JigItemAcceptRequestDto(
+        String requestId,
+        Boolean isAccept,
+        String memo,
         List<String> serialNos
 ) {
 }
