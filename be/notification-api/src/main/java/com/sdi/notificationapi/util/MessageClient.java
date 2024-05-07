c2dba17b412fac6e400e8eb2239130c90203d997
김용준 <rladydwns129@naver.com>
2024-05-07 12:59:02 +0900

    [Fix] #S10P31S105-115: apiServer 경로 수정
    Change-Id: Ibe2178583ba9c78937275c1cae2175e134a7f374

--- be/notification-api/src/main/java/com/sdi/notificationapi/util/MessageClient.java
+++ be/notification-api/src/main/java/com/sdi/notificationapi/util/MessageClient.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "messageClient", url = "${apis.api-base-url}") // 요청을 보낼 url 작성
 public interface MessageClient {
-    @PostMapping("/sse/send-message")
+    @PostMapping("/notification/sse/send-message")
     void sendMessage(@RequestBody MessageDto messageDto, @RequestHeader("Authorization") String accessToken);
 }
