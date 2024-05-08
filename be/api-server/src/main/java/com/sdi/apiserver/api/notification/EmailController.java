a76de5d85f7486d1856d0679178ec1feca01b272
김용준 <rladydwns129@naver.com>
2024-05-09 01:09:01 +0900

    [Feat] #S10P31S105-172: email 발송 기능 api서버 연결
    Change-Id: I2516ae0ef4b592dbc0d4aeae962019facaadbf4b

--- be/api-server/src/main/java/com/sdi/apiserver/api/notification/EmailController.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/notification/EmailController.java
@@ -1,38 +1,38 @@
 package com.sdi.apiserver.api.notification;
 
 import com.sdi.apiserver.api.notification.client.NotificationClient;
-import com.sdi.apiserver.api.notification.dto.request.MessageRequestDto;
+import com.sdi.apiserver.api.notification.dto.request.NotificationFcmInspectionRequestDto;
 import com.sdi.apiserver.util.Response;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
-@RequestMapping("/notification/sse")
+@RequestMapping("/v1/notification/email")
 @RequiredArgsConstructor
-public class SseController {
+public class EmailController {
     public static final String ACCESS_TOKEN_PREFIX = "Authorization";
     private final NotificationClient notificationClient;
 
-    @GetMapping(value = "/subscribe", produces = "text/event-stream")
-    SseEmitter subscribeSSE(HttpServletRequest request) {
-        return notificationClient.subscribeSSE(getAccessToken(request));
+    @PostMapping("/subscribe")
+    Response<Void> subscribeEmail(HttpServletRequest request) {
+        notificationClient.subscribeEmail(getAccessToken(request));
+        return Response.success();
+    }
+    @DeleteMapping("/unsubscribe")
+    Response<Void> unsubscribeEmail(HttpServletRequest request) {
+        notificationClient.unsubscribeEmail(getAccessToken(request));
+        return Response.success();
     }
 
-    @PostMapping(value = "/send-message")
-    Response<Void> sendMessage(@RequestBody MessageRequestDto messageRequestDto) {
-        return notificationClient.sendMessage(messageRequestDto);
-    }
-
-    @DeleteMapping("/disconnect")
-    Response<Void> disconnect(HttpServletRequest request) {
-        return notificationClient.disconnect(getAccessToken(request));
+    @PostMapping("/inspection")
+    Response<Void> sendInspectionEmail(@RequestBody NotificationFcmInspectionRequestDto dto) {
+        notificationClient.sendInspectionEmail(dto);
+        return Response.success();
     }
 
     private String getAccessToken(HttpServletRequest request) {
