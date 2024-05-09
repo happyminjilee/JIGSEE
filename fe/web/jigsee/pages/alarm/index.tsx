769f178f72358811172f3575360518f010496494
MinjiLEE <alswl9703@naver.com>
2024-05-09 23:36:59 +0900

    [Design] #S10P31S105-233: alarm조회 페이지 디자인 개선
    Change-Id: I5365d253c8ab9fa73319b54c16d24ce87206063f

--- fe/web/jigsee/pages/alarm/index.tsx
+++ fe/web/jigsee/pages/alarm/index.tsx
@@ -1,38 +1,14 @@
-import { Pagination, button, divider } from "@nextui-org/react";
+import { Pagination } from "@nextui-org/react";
 import { useState, useEffect } from "react";
 import Switch from "@mui/material/Switch";
 import alarmStyle from "@/styles/alarm.module.scss";
 import { useAlarmStore } from "@/store/ssestore";
 import LocalPostOfficeIcon from "@mui/icons-material/LocalPostOffice";
 import DraftsIcon from "@mui/icons-material/Drafts";
-import Modal from "@mui/material/Modal";
-import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
 import EngineerNav from "@/pages/engineer/navbar";
 import ManagerNav from "@/pages/manager/navbar";
-interface InfoAlarm {
-  checkStatus: boolean;
-  contentId: string;
-  id: number;
-  notificationStatus: string;
-  receiver: string;
-  sender: string;
-}
+
 export default function Alarm() {
-  const [openModal, setOpenModal] = useState(false);
-
-  const defaultInfoAlarm: InfoAlarm = {
-    checkStatus: false, // Default boolean value
-    contentId: "", // Default string value
-    id: 0, // Default number value
-    notificationStatus: "", // Default string value
-    receiver: "", // Default string value
-    sender: "", // Default string value
-  };
-
-  // alarm detail
-  const [cardItem, setCarditem] = useState<InfoAlarm>(defaultInfoAlarm);
-
   // switch checked 확인 변수
   const [checked, setChecked] = useState(true);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -40,21 +16,16 @@ export default function Alarm() {
   };
   // 알람 스토어 변수 들
   const { alarmId, uncheckednumber, uncheckedList, setUnchecked, setAlarmCheck } = useAlarmStore();
-  const { setAllalarm, all_alarams, alarmList, setPage, page } = useAlarmStore();
-  const checkClick = (ID: number, item: InfoAlarm) => {
+  const { setAllalarm, alarmList, setPage, page } = useAlarmStore();
+  const checkClick = (ID: number) => {
     console.log(ID);
     setAlarmCheck(ID);
-    setOpenModal(true);
-    setCarditem(item);
   };
-  // 모달 닫기 버튼
-  const closeModal = () => {
-    setOpenModal(false);
-  };
+
   // 알림 get test
   useEffect(() => {
     setUnchecked();
-  }, []);
+  }, [alarmId]);
   // 알림 get test
   useEffect(() => {
     setAllalarm();
@@ -84,7 +55,7 @@ export default function Alarm() {
       <div className={alarmStyle.bigcontainer}>
         <div className={alarmStyle.switch}>
           <div className={alarmStyle.switchlabel}>
-            {checked && <label>미확인 알림</label>}
+            {checked && <label>미확인 알림 {uncheckednumber}</label>}
             {checked === false && <label>전체 알림</label>}
             <Switch
               checked={checked}
@@ -96,16 +67,18 @@ export default function Alarm() {
 
         {checked ? (
           <div className={alarmStyle.container}>
-            <div className={alarmStyle.numberalarm}>미확인 알람 {uncheckednumber}</div>
             <div className={alarmStyle.scrollcontainer}>
               {uncheckedList.map((item, index) => (
                 <div
                   key={index}
                   className={alarmStyle.fullWidth}
-                  onClick={() => checkClick(item.id, item)}
+                  onClick={() => checkClick(item.id)}
                 >
-                  {item.notificationStatus} | 요청자{item.sender}
-                  {item.checkStatus === false && <LocalPostOfficeIcon color="primary" />}
+                  {item.notificationStatus}{" "}
+                  <div>
+                    요청자{item.sender}
+                    {item.checkStatus === false && <LocalPostOfficeIcon color="primary" />}
+                  </div>
                 </div>
               ))}
             </div>
@@ -113,11 +86,7 @@ export default function Alarm() {
         ) : (
           <div className={alarmStyle.container}>
             {alarmList.map((item, index) => (
-              <div
-                key={index}
-                className={alarmStyle.fullWidth}
-                onClick={() => checkClick(item.id, item)}
-              >
+              <div key={index} className={alarmStyle.fullWidth} onClick={() => checkClick(item.id)}>
                 {item.notificationStatus} | 요청자{item.sender}
                 {item.checkStatus === false && <LocalPostOfficeIcon color="primary" />}
                 {item.checkStatus === true && <DraftsIcon />}
@@ -129,72 +98,6 @@ export default function Alarm() {
           </div>
         )}
       </div>
-
-      {/* 디테일 보기 */}
-      <Modal
-        open={openModal} // Corrected from 'open'
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Box
-          sx={{
-            width: "100%",
-            height: "80%",
-            display: "flex",
-            alignItems: "start",
-            justifyContent: "center",
-          }}
-        >
-          {/* 닫기 아이콘 */}
-
-          <Box
-            sx={{
-              width: "500px",
-              height: "300px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "start",
-              justifyContent: "center",
-              backgroundColor: "var(--realwhite)",
-              border: "1px solid var(--samsungblue)",
-              borderRadius: "5px",
-            }}
-          >
-            id : {cardItem.id}
-            <Typography variant="h6" sx={{ marginTop: "5px" }}>
-              {" "}
-              요청 번호 : {cardItem.contentId}
-            </Typography>
-            <Typography sx={{ width: "100%", marginTop: "50px" }} align="right">
-              {" "}
-              요청자 : {cardItem.sender}
-            </Typography>
-            <Typography sx={{ width: "100%" }} align="right">
-              {" "}
-              수신인 : {cardItem.receiver}
-            </Typography>
-            <Typography color="primary" sx={{ width: "100%", marginTop: "30px" }} align="center">
-              {" "}
-              요청 사항{" "}
-            </Typography>
-            <Typography color="primary" sx={{ width: "100%" }} align="center" variant="h4">
-              {" "}
-              {cardItem.notificationStatus}
-            </Typography>
-          </Box>
-          <img
-            src="/images/delete_gray.svg"
-            alt="delete"
-            onClick={closeModal}
-            style={{ width: "50px", height: "50px" }}
-          ></img>
-        </Box>
-      </Modal>
     </>
   );
 }
