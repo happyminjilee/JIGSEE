40ccb7aa41d65690d8ae02b9713e87eb3cc450e1
차현철 <ckguscjf03@gmail.com>
2024-05-09 15:25:16 +0900

    [Fix] #S10P31S105-243: JigItem 응답 Dto 수정
    Change-Id: Ie065b7b55f1b963f02247a162fe80116c937cfba

--- be/api-server/src/main/java/com/sdi/apiserver/api/jig/dto/response/JigItemInventoryRequestDto.java
+++ be/api-server/src/main/java/com/sdi/apiserver/api/jig/dto/response/JigItemInventoryRequestDto.java
@@ -5,6 +5,6 @@ import java.util.List;
 public record JigItemInventoryRequestDto(
         List<JigItemInventory> list
 ) {
-    public record JigItemInventory(String mode, String count){
+    public record JigItemInventory(String model, String count){
     }
 }
