79ecd5338aa6746f59fb8d2ecf3ec8ff30e4a4e0
김용준 <rladydwns129@naver.com>
2024-05-12 22:59:54 +0900

    [Feat] S10P31S105-255: fcm flutter 연결
    Change-Id: I9128ab4b74ebfa8fdf7ed73873fdfe7f43fd6766

--- fe/mobile/jigsee/lib/main.dart
+++ fe/mobile/jigsee/lib/main.dart
@@ -8,18 +8,59 @@ import 'package:jigsee/components/navigate_animation.dart';
 import 'package:jigsee/pages/ocr_page.dart';
 import 'package:jigsee/api/user_auth.dart';
 
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("백그라운드 메시지 처리.. ${message.notification!.body!}");
+}
+
+void initializeNotification() async {
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(const AndroidNotificationChannel(
+      'high_importance_channel', 'high_importance_notification',
+      importance: Importance.max));
+
+  await flutterLocalNotificationsPlugin.initialize(const InitializationSettings(
+    android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+  ));
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final cameras = await availableCameras();
   final firstCamera = cameras.first;
-
+  await Firebase.initializeApp();
+  initializeNotification();
   runApp(ProviderScope(child: MyApp(camera: firstCamera)));
+  // runApp(const MyApp());
 }
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//
+//
+// }
 
 class MyApp extends StatelessWidget {
   final CameraDescription camera;
 
   const MyApp({Key? key, required this.camera}) : super(key: key);
+  // const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +94,122 @@ class MyApp extends StatelessWidget {
             return MaterialPageRoute(builder: (_) => const HomePage());
         }
       },
+    );
+  }
+}
+
+/*Hompage 클래스에 이식 필요
+*
+* */
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var messageString = "";
+
+  Future<String?> getMyDeviceToken() async {
+    final token = await FirebaseMessaging.instance.getToken();
+    print("내 디바이스 토큰: $token");
+    return token;
+  }
+
+  /*
+  * POST
+  * /api/v1/notification/fcm/token
+  * body => "token" : {deviceToken}
+  */
+  void sendTokenToServer(String deviceToken) async {
+
+  }
+
+  @override
+  void initState() {
+    Future<String?> token = getMyDeviceToken(); // device-token 가져옴
+    token
+        .then((value) => sendTokenToServer(value!)); // 가져온 deveice-token을 서버에 저장함
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      RemoteNotification? notification = message.notification;
+
+      if (notification != null) {
+        FlutterLocalNotificationsPlugin().show(
+          notification.hashCode,
+          notification.title,
+          notification.body,
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              'high_importance_channel',
+              'high_importance_notification',
+              importance: Importance.max,
+            ),
+          ),
+        );
+        setState(() {
+          messageString = message.notification!.body!;
+          print("Foreground 메시지 수신: $messageString");
+        });
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? message) {
+      if (message != null) {
+        RemoteNotification? notification = message.notification;
+        if (notification != null) {
+          FlutterLocalNotificationsPlugin().show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            const NotificationDetails(
+              android: AndroidNotificationDetails(
+                'high_importance_channel',
+                'high_importance_notification',
+                importance: Importance.max,
+              ),
+            ),
+          );
+        }
+      }
+    });
+
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? message) {
+      if (message != null) {
+        RemoteNotification? notification = message.notification;
+        if (notification != null) {
+          FlutterLocalNotificationsPlugin().show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            const NotificationDetails(
+              android: AndroidNotificationDetails(
+                'high_importance_channel',
+                'high_importance_notification',
+                importance: Importance.max,
+              ),
+            ),
+          );
+        }
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("메시지 내용: $messageString"),
+          ],
+        ),
+      ),
     );
   }
 }
