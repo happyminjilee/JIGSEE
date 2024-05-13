d4d8a5d6e34f9a27060b1ec33a6a4ab628ee13ce
MinjiLEE <alswl9703@naver.com>
2024-05-13 09:46:53 +0900

    [Feat] #S10P31S105-235: 현황판 디자인 초안 & k8 ok 응답
    Change-Id: I683ba891aacb28ea1b88c67f81a9897622876ffd

---
+++ fe/web/jigsee/components/dashboard/releasetable.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from "@nextui-org/react";
+
+interface Item {
+  name: string;
+  warehouse: number;
+  factory: number;
+}
+
+const list: Item[] = [
+  { name: "js1", warehouse: 159, factory: 6 },
+  { name: "js2", warehouse: 237, factory: 9 },
+  { name: "js3", warehouse: 262, factory: 16 },
+  { name: "js4", warehouse: 305, factory: 8 },
+  { name: "js5", warehouse: 12, factory: 2 },
+  { name: "js6", warehouse: 258, factory: 4 },
+];
+
+export default function Releasetable() {
+  return (
+    <>
+      {" "}
+      <Table
+        aria-label="Example table with client side sorting"
+        selectionMode="single"
+        color="primary"
+      >
+        <TableHeader>
+          <TableColumn key="name">모델</TableColumn>
+          <TableColumn key="warehouse">창고</TableColumn>
+          <TableColumn key="factory">공장</TableColumn>
+        </TableHeader>
+        <TableBody items={list}>
+          {(item) => (
+            <TableRow key={item.name}>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.warehouse}</TableCell>
+              <TableCell>{item.factory}</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </>
+  );
+}
