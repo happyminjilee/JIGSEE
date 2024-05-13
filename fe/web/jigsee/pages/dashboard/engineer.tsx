ff921bc651ee9ec2b5864ea8987f08e9c7b34457
주준형 <j40605@hanyang.ac.kr>
2024-05-13 10:45:26 +0900

    [Fix] #S10P31S105-268: url fix í현황판 component 추가
    Change-Id: I81fcea80e7821eda49744daa7622efff387ca91e

--- fe/web/jigsee/pages/dashboard/engineer.tsx
+++ fe/web/jigsee/pages/dashboard/engineer.tsx
@@ -1,8 +1,26 @@
 import Navbar from "@/pages/engineer/navbar";
 import styled from "@/styles/dashboard/engineer.module.scss";
 import Report from "@/components/dashboard/report";
+import MyWoList from "@/components/dashboard/myWo";
+import {useEffect, useState} from "react";
+import {useUserWoListStore} from "@/store/workorderstore";
 
 export default function Engineerdashboard() {
+  const {list, fetchUserWo} = useUserWoListStore()
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+        const employeeNo = localStorage.getItem("employeeNo") || ""
+        const name = localStorage.getItem("name") || ""
+        fetchUserWo(employeeNo, name, 1, 5)
+            .then((res) => {
+                console.log('res', res)
+            })
+            .catch((error) => {
+                console.log('error', error)
+            })
+      }
+  }, []);
   return (
     <>
       <Navbar />
@@ -16,7 +34,9 @@ export default function Engineerdashboard() {
         </div>
         <div className={styled.bottom}>
           <div className={styled.method}>지그 점검항목 수정</div>
-          <div className={styled.wo}>내가 신청한 wo</div>
+          <div className={styled.wo}>
+              <MyWoList/>
+          </div>
         </div>
       </div>
     </>
