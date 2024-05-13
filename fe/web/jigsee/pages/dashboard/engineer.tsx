b064904a6ec5bccaabaebafa9c3b19d6ba3ed1e4
MinjiLEE <alswl9703@naver.com>
2024-05-13 12:35:35 +0900

    [Feat] #S10P31S105-298: engineer dashboard 디자인
    Change-Id: I8111dcee19bb36fffdfd2d6ccbe87f0f6c8bb225

--- fe/web/jigsee/pages/dashboard/engineer.tsx
+++ fe/web/jigsee/pages/dashboard/engineer.tsx
@@ -3,9 +3,11 @@ import styled from "@/styles/dashboard/engineer.module.scss";
 import Report from "@/components/dashboard/report";
 import ReleaseTable from "@/components/dashboard/releasetable";
 import dynamic from "next/dynamic";
+import Editmethod from "@/components/dashboard/editedmethod";
 const DonutChart = dynamic(() => import("@/components/dashboard/repairdonut"), {
   ssr: false, // 이 옵션은 서버 사이드 렌더링을 비활성화합니다.
 });
+const Lifetime = dynamic(() => import("@/components/dashboard/lifetime"), { ssr: false });
 export default function Engineerdashboard() {
   return (
     <>
@@ -17,13 +19,16 @@ export default function Engineerdashboard() {
         <div className={styled.middle}>
           <div className={styled.jiglocation}>
             <ReleaseTable />
+            <Lifetime />
           </div>
           <div className={styled.percent}>
             <DonutChart />
           </div>
         </div>
         <div className={styled.bottom}>
-          <div className={styled.method}>지그 점검항목 수정</div>
+          <div className={styled.method}>
+            <Editmethod />
+          </div>
           <div className={styled.wo}>내가 신청한 wo</div>
         </div>
       </div>
