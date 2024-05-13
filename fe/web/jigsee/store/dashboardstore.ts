9b533fea7eb31e82de034d5e151a00f2e789d033
MinjiLEE <alswl9703@naver.com>
2024-05-13 22:28:12 +0900

    [Feat] #S10P31S105-298: 지그 위치에 따른 갯수 api
    Change-Id: Ia7513e1675f5b37ee623a54c4f0d9de1ec8d95c0

--- fe/web/jigsee/store/dashboardstore.ts
+++ fe/web/jigsee/store/dashboardstore.ts
@@ -1,13 +1,22 @@
 import { create } from "zustand";
-import { getMonthJig } from "@/pages/api/dashboard";
+import { getMonthJig, getJigcount } from "@/pages/api/dashboard";
 
+interface Jiglocation {
+  model: string;
+  countReady: number;
+  countWarehouse: number;
+}
 interface dashboardstore {
   deleted: number;
   change: number;
   request: number;
   finish: number;
   getJignumbers: () => Promise<void>;
+  maxcount: number;
+  modelscount: Jiglocation[];
+  getJigcounts: () => Promise<void>;
 }
+
 export const useDashboardstore = create<dashboardstore>((set) => ({
   deleted: 0,
   change: 0,
@@ -22,5 +31,12 @@ export const useDashboardstore = create<dashboardstore>((set) => ({
       request: data.countRepairRequest,
       finish: data.countRepairFinish,
     });
+  },
+  maxcount: 0,
+  modelscount: [],
+  getJigcounts: async () => {
+    const data = await getJigcount();
+    console.log("jig locaaaa", data);
+    set({ modelscount: data.jigModelCountList, maxcount: data.maxCount });
   },
 }));
