916951b7db5572efcd5b492e5d71839a2089d85e
박수형 <nastorond@kakao.com>
2024-05-14 16:34:17 +0900

    [Feat] #S10P21S002-310 상단 크롬탭 로고랑 이름 설정
    Change-Id: Ie64fdfc60e4e9b970de9f3496aafaa5a9aa19e15

--- fe/web/jigsee/pages/_app.tsx
+++ fe/web/jigsee/pages/_app.tsx
@@ -1,11 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <>
+      <Head>
+        <title>JIG:SEE</title>
+        <meta name="description" content="This is a description of my app" />
+        <link rel="icon" href="/images/icon.png" />
+      </Head>
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </>
   );
 }
