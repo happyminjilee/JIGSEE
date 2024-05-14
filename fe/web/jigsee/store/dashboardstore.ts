23993369a8092e9a8b38f37de8dc5bfa590f48e2
MinjiLEE <alswl9703@naver.com>
2024-05-14 12:41:29 +0900

    [Feat] #S10P31S105-298: 점검항목 지그 api 완성
    Change-Id: I21f02c342b433bfae29fb30716d47c4501426109

--- fe/web/jigsee/store/dashboardstore.ts
+++ fe/web/jigsee/store/dashboardstore.ts
@@ -1,23 +1,37 @@
 import { create } from "zustand";
-import { getMonthJig, getJigcount } from "@/pages/api/dashboard";
+import { getMonthJig, getJigcount, updateChecked } from "@/pages/api/dashboard";
 
 interface Jiglocation {
   model: string;
   countReady: number;
   countWarehouse: number;
 }
+interface CheckItem {
+  content: string;
+  standard: string;
+}
+interface Updated {
+  model: string;
+  checkItems: CheckItem[];
+}
 interface dashboardstore {
+  // 지그 수리, 폐기 수
   deleted: number;
   change: number;
   request: number;
   finish: number;
   getJignumbers: () => Promise<void>;
+  // 지그 창고, 현장대기 수
   maxcount: number;
   modelscount: Jiglocation[];
   getJigcounts: () => Promise<void>;
+  //점검항목 수정된 지그 리스트
+  getJigupdated: () => Promise<void>;
+  updatedList: Updated[];
 }
 
 export const useDashboardstore = create<dashboardstore>((set) => ({
+  // 지그 수리, 폐기 수
   deleted: 0,
   change: 0,
   request: 0,
@@ -32,11 +46,17 @@ export const useDashboardstore = create<dashboardstore>((set) => ({
       finish: data.countRepairFinish,
     });
   },
+  // 지그 창고, 현장대기 수
   maxcount: 0,
   modelscount: [],
   getJigcounts: async () => {
     const data = await getJigcount();
     console.log("jig locaaaa", data);
     set({ modelscount: data.jigModelCountList, maxcount: data.maxCount });
+  },
+  updatedList: [],
+  getJigupdated: async () => {
+    const data = await updateChecked();
+    set({ updatedList: data.updatedJigList });
   },
 }));
