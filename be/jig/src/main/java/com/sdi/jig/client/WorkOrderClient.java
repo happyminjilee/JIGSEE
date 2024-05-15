58db4864c37c2b050e6266074a8072ab490a972e
강성범 <qkfka9045@gmail.com>
2024-05-15 17:26:17 +0900

    [Feat] #S10P31S105: wo 생성 로직 추가
    Change-Id: I437c3e849e1fc199d6a9f5f3d8a4c82dd15140ea

--- be/jig/src/main/java/com/sdi/jig/client/WorkOrderClient.java
+++ be/jig/src/main/java/com/sdi/jig/client/WorkOrderClient.java
@@ -1,10 +1,12 @@
 package com.sdi.jig.client;
 
+import com.sdi.jig.dto.request.WorkOrderAutoCreateRequestDto;
 import com.sdi.jig.dto.response.WorkOrderCountResponseDto;
 import com.sdi.jig.util.Response;
 import com.sdi.jig.util.TokenHeader;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,4 +17,7 @@ public interface WorkOrderClient {
     Response<WorkOrderCountResponseDto> countRepairRequest(@RequestHeader(name = TokenHeader.AUTHORIZATION) String accessToken,
                                                            @RequestParam(name = "year") Integer year,
                                                            @RequestParam(name = "month") Integer month);
+
+    @PostMapping("/work-order/auto")
+    Response<Void> auto(WorkOrderAutoCreateRequestDto dto);
 }
---
+++ be/jig/src/main/java/com/sdi/jig/dto/request/WorkOrderAutoCreateRequestDto.java
@@ -0,0 +1,12 @@
+package com.sdi.jig.dto.request;
+
+import java.util.List;
+
+public record WorkOrderAutoCreateRequestDto(
+        List<String> serialNos
+) {
+
+    public static WorkOrderAutoCreateRequestDto from(List<String> serialNos){
+        return new WorkOrderAutoCreateRequestDto(serialNos);
+    }
+}
