89108045d7da2c0a801017b24ad08f5e5fc5b31b
차현철 <ckguscjf03@gmail.com>
2024-05-17 13:42:53 +0900

    [Feat] #S10P31S105: jig서버 그래프 조회 api 수정
    Change-Id: I0cdf623376a7deccdfc907ea4f7b9de559b9e68c

--- be/jig/src/main/java/com/sdi/jig/application/JigService.java
+++ be/jig/src/main/java/com/sdi/jig/application/JigService.java
@@ -32,7 +32,7 @@ public class JigService {
     private final int PRODUCTION_PROFIT = 5;
     private final int FACILITY_HOUR_OPERATING_COST = 3000;
     private final int BREAKEDOWN_COST = 5000;
-    private final int MAINTANANCE_COST = 3000;
+    private final int MAINTANANCE_COST = 1000;
     private final int MONTH_DAY = Calendar.getInstance().getActualMaximum(Calendar.DAY_OF_MONTH);
     private final int DAY_TIME = 24;
     private final int CHECK_PEOPLE = 20;
